69c5d06bb21618d58a4857f64500b5552a616527
Akhilesh Halekote Mohan <akhileshhalekotemohan@gmail.com>
2026-03-06 13:16:12 -0500

    Adding server.py

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from Flask import flask
+from Flask import flask, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route('\emotionDetector')
+@app.route('/emotionDetector')
 def emotion_analysis():
     text = request.form['textToAnalyze']
     res = emotion_detector(text)
+    request.form['system_response'] = r the given statement, the system response is 'anger'': {res['anger']}, 'disgust': {res['disgust']}, 'fear': {res['fear']}, 'joy': {res['joy']} and 'sadness': {res['sadness']}. The dominant emotion is {res['dominant_emotion']}."
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
